c5613cf6857568040aabf31355f0b85dbc858de2
gajendra-shrestha <gajendra.nyc@gmail.com>
2023-09-27 10:28:35 -0400

    added catalog and site header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
